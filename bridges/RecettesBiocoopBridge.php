fed2158d3709e314edc88ddc4b256a3abce698dd
Florent VIOLLEAU <floviolleau@laposte.net>
2025-11-23 03:02:43 +0100

    [RecettesBioocop] Update dom scrapping

--- bridges/RecettesBiocoopBridge.php
+++ bridges/RecettesBiocoopBridge.php
@@ -2,11 +2,11 @@
 class RecettesBiocoopBridge extends BridgeAbstract {
 
 	const NAME = "Recettes de Biocoop Combourg, Dol de Bretagne et Tinteniac";
-	const URI = 'https://www.biocooplechatbiotte.com/recettes-le-chat-biotte-combourg/1.html';
+	const URI = 'https://www.biocooplechatbiotte.com/recettes';
 	const DESCRIPTION = "Fetches the latest Recipes of Le Chat Biotté Combourg, Dol-De-Bretagne, Tinteniac";
 	const MAINTAINER = 'floviolleau';
-	const PARAMETERS = array();
-	const CACHE_TIMEOUT = 0; // 7200;
+	const PARAMETERS = [];
+	const CACHE_TIMEOUT = 7200;
 
 	private function clean($string) {
 		$string = str_replace(' ', '-', $string);
@@ -18,15 +18,18 @@ class RecettesBiocoopBridge extends BridgeAbstract {
 
 		$html = getSimpleHTMLDOM($uri) or returnServerError('Could not request ' . $uri);
 
-		$containerDom = $html->find('.listeRecette', 0);
-		$publicationNumberDom = $containerDom->find('.weInnerItem');
+		$containerDom = $html->find('.liste-page.recette', 0);
+		$publicationNumberDom = $containerDom->find('.content');
 		foreach($publicationNumberDom as $key => $value) {
-			$publicationNumberTitle = $value->find('a.h3.titre', 0)->innerText();
+			$publicationNumberTitle = $value->find('.side-content h2 a span', 1)->innertext;
+
 			$beginningUrl = 'https://www.biocooplechatbiotte.com';
-			$publicationNumberUrl = $beginningUrl . $value->find('a.h3.titre', 0)->href;
-			$publicationNumberCategory = $value->find('a.btn.btn-default.btnListe.categorie', 0)->innerText();
+            $urlItem = $beginningUrl . $value->find('.side-content .lien-detail', 0)->href;
+
+			$publicationNumberCategory = $value->find('.side-content .badge', 0)->innertext;
 			$message = "Recette biocoop : [$publicationNumberCategory] $publicationNumberTitle";
-			$item['uri'] = $publicationNumberUrl;
+
+			$item['uri'] = $urlItem;
 			$item['title'] = $message;
 			$item['author'] = 'floviolleau';
 			$item['content'] = $message;
